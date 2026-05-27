10b7ee004b632a83603dde9ae521135f6f90a34a
trantronghieuvn <trantronghieu.vn@gmail.com>
2026-05-27 09:25:11 +0700

    Fix links in index

--- FunctionWords/FunctionWords.bat
+++ FunctionWords/FunctionWords.bat
@@ -7,4 +7,4 @@ timeout /t 3 /nobreak >nul
 
 echo Dang mo Chrome...
 :: Thêm cờ --disable-web-security chỉ dùng cho môi trường dev để test (Lưu ý: Không dùng khi duyệt web thông thường)
-start chrome --incognito --use-fake-ui-for-media-stream --disable-web-security --user-data-dir="C:/temp-chrome-dev" --unsafely-treat-insecure-origin-as-secure="http://localhost:8000" "http://localhost:8000/FunctionWordsMastery.html"
+start chrome --incognito --use-fake-ui-for-media-stream --disable-web-security --user-data-dir="C:/temp-chrome-dev" --unsafely-treat-insecure-origin-as-secure="http://localhost:8000" "http://localhost:8000/FunctionWords.html"

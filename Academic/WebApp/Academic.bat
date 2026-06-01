055ac1273a47466444e1c7a37cc21ecae87a1bfc
trantronghieuvn <trantronghieu.vn@gmail.com>
2026-06-01 23:08:48 +0700

    Update batch
    Update batch

--- Academic/WebApp/Academic.bat
+++ Academic/WebApp/Academic.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Dang khoi dong Hoc Tieng Anh...
+echo Dang khoi dong Academic...
 start cmd /k "python -m http.server 8001"
 timeout /t 2 /nobreak >nul
-start chrome --incognito --use-fake-ui-for-media-stream --unsafely-treat-insecure-origin-as-secure="http://localhost:8001" "http://localhost:8001/HocTiengAnh.html"
+start chrome --incognito --use-fake-ui-for-media-stream --unsafely-treat-insecure-origin-as-secure="http://localhost:8001" "http://localhost:8001/Academic.html"
